cd70f147f80a483782afb844e5002e930dde218a
altamanquez <2123100467@soy.utj.edu.mx>
2026-03-13 20:35:57 -0600

    avr k pdo

--- resources/views/layouts/cliente.blade.php
+++ resources/views/layouts/cliente.blade.php
@@ -201,7 +201,7 @@
             <a href="/" class="logo serif" style="letter-spacing: 4px; font-weight: 400;">SOLARE</a>
             <nav class="nav-menu">
                 <a href="/" class="nav-link">Inicio</a>
-                <a href="/catalogo" class="nav-link">Colecciones</a>
+                <a href="{{ route('catalogo') }}" class="nav-link">Colecciones</a>
                 <a href="/carrito" class="nav-link" style="position: relative; display: flex; align-items: center; padding: 10px;">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width: 48px; height: 48px;">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
